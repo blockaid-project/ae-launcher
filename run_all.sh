cdbe0c40eec68607d19d793bf65c92d510a95569
Wen Zhang <zhangwen@cs.berkeley.edu>
2022-04-14 16:22:51 -0700

    configurable region and ami

--- run_all.sh
+++ run_all.sh
@@ -12,18 +12,22 @@ then
   echo "AWS credentials not found..."
   read -r -p 'AWS Access Key ID: ' access_key_id
   read -r -p 'AWS Secret Access Key: ' secret_access_key
-  read -r -p 'AWS region (e.g., us-west-2): ' region
+  read -r -p 'AWS region [default: us-east-2]: ' region
+  region=${region:-us-east-2}
+  read -r -p 'EC2 AMI ID [default: ami-01457e9b6b7cdee4e]: ' ami_id
+  ami_id=${ami_id:-ami-01457e9b6b7cdee4e}
 
 tee "$CREDENTIALS_FILE" <<EOF >/dev/null
 export AWS_REGION=${region}
 export AWS_ACCESS_KEY=${access_key_id}
 export AWS_SECRET_KEY=${secret_access_key}
+export AMI_ID=${ami_id}
 EOF
 fi
 
 source "$CREDENTIALS_FILE"
 
-params=()
+params=("-e" "ami_id=$AMI_ID")
 [[ -v TEST_RUN ]] && params+=("-e" "test_run=1")
 ansible-playbook -f 6 "${params[@]}" launch.yml
 
