1696edb82ad79b067dc3322bc9a99932e063f3ae
Wen Zhang <zhangwen@cs.berkeley.edu>
2022-04-14 00:45:21 -0700

    fix run script

--- run_all.sh
+++ run_all.sh
@@ -14,7 +14,7 @@ then
   read -r -p 'AWS Secret Access Key: ' secret_access_key
   read -r -p 'AWS region (e.g., us-west-2): ' region
 
-tee newfile <<EOF >/dev/null
+tee "$CREDENTIALS_FILE" <<EOF >/dev/null
 export AWS_REGION=${region}
 export AWS_ACCESS_KEY=${access_key_id}
 export AWS_SECRET_KEY=${secret_access_key}
@@ -25,6 +25,6 @@ source "$CREDENTIALS_FILE"
 
 params=()
 [[ -v TEST_RUN ]] && params+=("-e" "test_run=1")
-ansible -f 6 "${params[@]}" launch.yml
+ansible-playbook -f 6 "${params[@]}" launch.yml
 
 ./plot/produce_plot_comparisons.sh
