486546a4bfff4251c6d2f9b804d501a10acb1a19
Wen Zhang <zhangwen@cs.berkeley.edu>
2022-04-14 11:50:38 -0700

    more plotting

--- plot/produce_plot_comparisons.sh
+++ plot/produce_plot_comparisons.sh
@@ -6,5 +6,7 @@ cd /data
 
 /app/experiments/plot/plot_fetch_pdf.R ./experiments
 /app/experiments/plot/make_plt_table.py ./experiments --no-siunitx > /data/plt_table.tex
+/app/experiments/plot/aggregate_winners.py ./experiments > /data/winners.csv
+/app/experiments/plot/plot_winners_pdf.R /data/winners.csv
 
 pdflatex /app/plot/all_plots.tex
