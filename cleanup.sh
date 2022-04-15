3069d38f9acb64487cd5fd96d0e273607f8565be
Wen Zhang <zhangwen@cs.berkeley.edu>
2022-04-14 17:04:57 -0700

    fix

--- cleanup.sh
+++ cleanup.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd "$SCRIPT_DIR"
 source set_credentials.sh
 ansible-playbook -f 6 "${params[@]}" cleanup.yml
 
